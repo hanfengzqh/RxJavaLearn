e9266bc1fa8094abc5e605de079210c6538090e4
hanfengzqh <1017459102@qq.com>
2017-09-09 19:47:38 +0800

    nohttp init

---
+++ nohttpdemo/src/main/java/com/zqh/rxjava/nohttpdemo/app/NoHttpApp.java
@@ -0,0 +1,15 @@
+package com.zqh.rxjava.nohttpdemo.app;
+
+import android.app.Application;
+
+/**
+ * Created by zqh on 2017/9/9.
+ */
+
+public class NoHttpApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
